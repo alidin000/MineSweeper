f7300c6574421513e98819ce18ff29360c281a36
Alidin Abylkasym uulu <alidinabylkasymuulu03@gmail.com>
2022-07-24 21:01:43 +0200

    TimeCount class created

---
+++ src/Timecount.java
@@ -0,0 +1,61 @@
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Timecount extends JPanel {
+    Timer t;
+    int score = 0;
+    int second= 0;
+    int minute= 0;
+    String Minutetemp = "00";
+    String Secondtemp = "00";
+    JLabel g = new JLabel();
+    public Timecount()
+    {
+        JFrame n = new JFrame();
+        g.setText(Minutetemp+":"+Secondtemp);
+        g.setForeground(Color.red);
+        n.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        n.setSize(600,600);
+        SimpleTimer();
+        t.start();
+        this.add(g);
+        n.add(this);
+        n.setVisible(true);
+    }
+    public void SimpleTimer()
+    {
+        t = new Timer(1000,(ActionListener) new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            second++;
+            score ++;
+            if(second == 60)
+            {
+                minute++;
+                second = 0;
+            }
+            if(minute < 10)
+                Minutetemp = "0"+minute;
+            else 
+                Minutetemp =""+ minute;
+            
+            if(second < 10)
+                Secondtemp = "0"+second;
+            else 
+                Secondtemp =""+ second;
+            g.setText(Minutetemp+":"+Secondtemp);
+        }
+        });
+    }
+    public static void main(String[] args) {
+        new Timecount();
+    }
+}
