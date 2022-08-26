c04695d8892a92f7d2f5a8213b1c759a5aa824df
Alidin Abylkasym uulu <alidinabylkasymuulu03@gmail.com>
2022-08-26 18:51:32 +0200

    moving constants to another class

---
+++ src/Constants.java
@@ -0,0 +1,36 @@
+import javax.swing.ImageIcon;
+
+public class Constants {
+    private ImageIcon flagImage = new ImageIcon("images/flag.png");
+    private ImageIcon bombImage = new ImageIcon("images/bomb.png");
+    private ImageIcon omg = new ImageIcon("images/omg.png");
+    private ImageIcon thumbsUp = new ImageIcon("images/thumbsUp.png");
+
+    public Constants()
+    {
+
+    }
+
+    public ImageIcon getFlagImg()
+    {
+        ImageIcon temp = flagImage;
+        return temp;
+    }
+
+    public ImageIcon getBombImg()
+    {
+        ImageIcon temp = bombImage;
+        return temp;
+    }
+    public ImageIcon getOmgImg()
+    {
+        ImageIcon temp = omg;
+        return temp;
+    }
+
+    public ImageIcon getThumbImg()
+    {
+        ImageIcon temp = thumbsUp;
+        return temp;
+    }
+}
